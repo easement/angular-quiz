0ce66f27ae923bea6c98a39e216d7b092a2bac52
Matt Billings <mattbillings@gmail.com>
2014-04-18 16:40:17 -0400

    adding answers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,59 +42,49 @@ angular
         'term': 'DAU',
         'answers': {
             'a': 'Daily Active Users',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Direct Advertiser Utility',
+            'c': 'Default Advertisement Unit',
+            'd': 'Daily Audit Utility'
           },
         'answer': 'a'
       },
       {
-        'term': 'MAU',
-        'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'Monthly Active Users',
-            'd': 'wrong'
-          },
-        'answer': 'c'
-      },
-      {
         'term': 'Reach',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'Monthly Reach'
+            'a': 'Total devices with app launch',
+            'b': 'Collection of CDN Zones',
+            'c': 'Current users per advertiser',
+            'd': 'Representation of the full advertising potential of Tapjoy'
           },
         'answer': 'd'
       },
       {
         'term': 'DUV (doves)',
         'answers': {
-            'a': 'Daily Unique Viewers',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'a': 'Daily Unique Viewers: A unique count of devices (UDID) that were actually exposed to an ad in a partner app or on Tapjoy.com',
+            'b': 'Daily Upvote Value: The number of upvotes on the offerwall',
+            'c': 'Dollar Unit Value: Virtual currency ratio to dollar. Used in stabilizing in-game economies',
+            'd': 'Dollar Updated Vector: Trending in virtual currency economy. Primarily used to determine when to run in-game currency sales'
           },
         'answer': 'a'
       },
       {
         'term': 'MUV',
         'answers': {
-            'a': 'wrong',
-            'b': 'Monthly Unique Viewers',
-            'c': 'wrong',
-            'd': 'wrong'
+            'a': 'Monthly Upvote Value: The number of upvotes in a partner app or on Tapjoy.com',
+            'b': 'Monthly Unique Viewers: A unique count of devices (UDID) that were actually exposed to an ad in a partner app or on Tapjoy.com',
+            'c': 'Money Unit Value: Virtual currency ratio to dollar. Used in stabilizing in-game economies',
+            'd': 'Money Updated Vector: Trending in virtual currency economy. Primarily used to determine when to run in-game currency sales'
           },
         'answer': 'b'
       },
       {
         'term': 'DUC (ducks)',
         'answers': {
-            'a': 'wrong',
-            'b': 'Daily Unique Converters',
-            'c': 'wrong',
-            'd': 'wrong'
+            'a': 'Deductible Unit Currency: Aggregate amount of charge back to advertisers due to fraud or missed conversions',
+            'b': 'Daily Unique Converters: A unique count of devices (UDID) that completed an offer in a partner app or on Tapjoy.com.',
+            'c': 'Duck Under Cover: That one scene in Howard the Duck, the movie',
+            'd': 'Distributed Unit Count:'
           },
         'answer': 'b'
       },
@@ -102,19 +92,19 @@ angular
         'term': 'MUC',
         'answers': {
             'a': 'Monthly Unique Converters',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Monthy Unit Count: ',
+            'c': 'Maximum Unit Count:',
+            'd': 'Main Unit Characteristics:'
           },
         'answer': 'a'
       },
       {
         'term': 'OW',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'Offerwall'
+            'a': 'OW, The Humanity',
+            'b': 'OW, The Pain',
+            'c': 'OW, ... of Warcraft',
+            'd': 'The Offerwall'
           },
         'answer': 'd'
       },
@@ -122,9 +112,9 @@ angular
         'term': 'Featured',
         'answers': {
             'a': 'Featured Ads',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Featured Creatures',
+            'c': 'Featured Clients',
+            'd': 'Featured Innovations'
           },
         'answer': 'a'
       },
@@ -132,38 +122,48 @@ angular
         'term': 'Display',
         'answers': {
             'a': 'Display Ads',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Display Menu',
+            'c': 'Display Monitor',
+            'd': 'Display TV'
           },
         'answer': 'a'
       },
       {
         'term': 'TJC',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
+            'a': 'Tapjoy Conversion',
+            'b': 'Total Jump Comscore',
             'c': 'Tapjoy.com',
-            'd': 'wrong'
+            'd': 'Tabular Junction Compartment'
+          },
+        'answer': 'c'
+      },
+      {
+        'term': 'MAU',
+        'answers': {
+            'a': 'Manual Automated Unit',
+            'b': 'Maximum Allowed Usage',
+            'c': 'Monthly Active Users',
+            'd': 'Minimum Allowed Users'
           },
         'answer': 'c'
       },
       {
         'term': 'CPI (PPI)',
         'answers': {
-            'a': 'wrong',
+            'a': 'Current Placement Indicator',
             'b': 'Cost per Install',
-            'c': 'wrong',
-            'd': 'wrong'
+            'c': 'Currency per Intersisital',
+            'd': 'Cost per Innnovation'
           },
         'answer': 'b'
       },
       {
         'term': 'CPE (PPE)',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
+            'a': 'Currency Per Event',
+            'b': 'Current Price Evaulation',
+            'c': 'Copy Pasta Error',
             'd': 'Cost per Engagement'
           },
         'answer': 'd'
@@ -172,19 +172,19 @@ angular
         'term': 'CPA',
         'answers': {
             'a': 'Cost per Action',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Current Project Availability',
+            'c': 'Copy Pasta Arror',
+            'd': 'Cross Platform Availability'
           },
         'answer': 'a'
       },
       {
         'term': 'CPV',
         'answers': {
-            'a': 'wrong',
+            'a': 'Current Page Views',
             'b': 'Cost per View',
-            'c': 'wrong',
-            'd': 'wrong'
+            'c': 'Copy Pasta Verror',
+            'd': 'Current Prospect Vector'
           },
         'answer': 'b'
       },
@@ -192,28 +192,28 @@ angular
         'term': 'CPC',
         'answers': {
             'a': 'Cost per Click',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Current Prosepctive Clients',
+            'c': 'Cost per Customer',
+            'd': 'Copy Pasta Curry'
           },
         'answer': 'a'
       },
       {
         'term': 'Impression',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
+            'a': 'Only one chance to make a bad first one',
+            'b': 'A Monet painting',
             'c': 'Ad Impression',
-            'd': 'wrong'
+            'd': 'Wrong'
           },
         'answer': 'c'
       },
       {
         'term': 'OWO',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
+            'a': 'Owl With Ostrich',
+            'b': 'Only Working Outlet',
+            'c': 'Optimal Workflow Offer',
             'd': 'Offerwall Open'
           },
         'answer': 'd'
@@ -222,9 +222,9 @@ angular
         'term': 'OWO Rate',
         'answers': {
             'a': 'Offerwall Open Rate',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Owl With Ostrich per Blue Moon',
+            'c': 'Optimal Workflow Offer Rate',
+            'd': 'Oh? What? Oh! Rating'
           },
         'answer': 'a'
       },
@@ -232,38 +232,28 @@ angular
         'term': 'Rank',
         'answers': {
             'a': 'Offerwall Rank',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Ad Sort Order',
+            'c': 'Ad Ranking',
+            'd': 'App Sort order'
           },
         'answer': 'a'
-      },
-      {
-        'term': 'Click',
-        'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'Click',
-            'd': 'wrong'
-          },
-        'answer': 'c'
       },
       {
         'term': 'Convs',
         'answers': {
             'a': 'Conversion',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Converse Tennis Shoes',
+            'c': 'Conversation',
+            'd': 'Cons Vs. Pros'
           },
         'answer': 'a'
       },
       {
         'term': 'CTR',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
+            'a': 'Capture The Rookie',
+            'b': 'Cost Through Revenue',
+            'c': 'Constant Target Return',
             'd': 'Click Through Rate'
           },
         'answer': 'd'
@@ -272,48 +262,48 @@ angular
         'term': 'CVR',
         'answers': {
             'a': 'Conversion Rate',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Constant Vector Revenue',
+            'c': 'Current Video Release',
+            'd': 'Conversation Rate'
           },
         'answer': 'a'
       },
       {
         'term': 'ACR',
         'answers': {
-            'a': 'wrong',
+            'a': 'All Country Referrer',
             'b': 'Action Conversion Rate',
-            'c': 'wrong',
-            'd': 'wrong'
+            'c': 'Applicable Cost Rating',
+            'd': 'Accurate Capture Rate'
           },
         'answer': 'b'
       },
       {
         'term': 'Spend',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'Ad Spend',
-            'd': 'wrong'
+            'a': 'wrong (intentional)',
+            'b': 'wrong (not intentional)',
+            'c': 'Ad Spend: The amount of money spent by advertisers on conversions',
+            'd': 'wrong (who knows at this point)'
           },
         'answer': 'c'
       },
       {
         'term': 'Earnings',
         'answers': {
-            'a': 'wrong',
+            'a': 'Customer Earnings',
             'b': 'Publisher Earnings',
-            'c': 'wrong',
-            'd': 'wrong'
+            'c': 'Consumer Earnings',
+            'd': 'Losses'
           },
         'answer': 'b'
       },
       {
         'term': 'GR',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
+            'a': 'Great Brittian',
+            'b': 'Grand Royal',
+            'c': 'Gran Tourismo',
             'd': 'Gross Revenue'
           },
         'answer': 'd'
@@ -321,9 +311,9 @@ angular
       {
         'term': 'TJR',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
+            'a': 'Total Junction Reporting',
+            'b': 'Topical Journaled Revenue',
+            'c': 'Temporary Jaundice Reaction',
             'd': 'Tapjoy Revenue'
           },
         'answer': 'd'
@@ -332,19 +322,19 @@ angular
         'term': 'eCPM',
         'answers': {
             'a': 'Effective Cost per Thousand Impressions',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'everybody loves raymond Copied Pirated Materials',
+            'c': 'encompassment',
+            'd': 'eCapital Premium Merchant'
           },
         'answer': 'a'
       },
       {
         'term': 'eCPM OW',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'Effective Cost per Thousand Opens',
-            'd': 'wrong'
+            'a': 'Pick c',
+            'b': 'One down',
+            'c': 'Effective Cost per Thousand Offerwall Opens ',
+            'd': 'One up'
           },
         'answer': 'c'
       },
@@ -352,18 +342,18 @@ angular
         'term': 'UDID',
         'answers': {
             'a': 'Device id',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Oh no UDID not',
+            'c': 'Unique Diplomatic International Directory',
+            'd': 'Unicode Data Indicator Device'
           },
         'answer': 'a'
       },
       {
         'term': 'ARPC',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
+            'a': 'AARP RPC',
+            'b': 'Amalgamated Resource Point of Contact',
+            'c': 'Associated Report Placement eCapacity',
             'd': 'Avg Revenue Per Conversion'
           },
         'answer': 'd'
@@ -371,29 +361,29 @@ angular
       {
         'term': 'ARPU',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
+            'a': 'Aggregated Rubies Per Unit',
+            'b': 'Agnostic Reuse of Perplexity Unification Theory',
             'c': 'Avg Revenue Per User',
-            'd': 'wrong'
+            'd': 'Automated Report Per UDIDs'
           },
         'answer': 'c'
       },
       {
         'term': 'ARPPU',
         'answers': {
-            'a': 'wrong',
+            'a': 'Application Resource Per Premium User',
             'b': 'Avg Revenue Per Paying User',
-            'c': 'wrong',
-            'd': 'wrong'
+            'c': 'Average Reaction Per Pasta Utopia',
+            'd': 'Active Resource Placement per User'
           },
         'answer': 'b'
       },
       {
         'term': 'ARPDAU',
         'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'wrong',
+            'a': 'Alphabet Soup',
+            'b': 'All Resource Paying Deemed Accurate by UDID',
+            'c': 'ARPPU With Direction',
             'd': 'Avg Revenue Per Daily Active User'
           },
         'answer': 'd'
@@ -402,21 +392,11 @@ angular
         'term': 'Show Rate',
         'answers': {
             'a': 'Offer Show Rate',
-            'b': 'wrong',
-            'c': 'wrong',
-            'd': 'wrong'
+            'b': 'Opposite of Hide Rate',
+            'c': 'Short Hueristic Offerwall Rate',
+            'd': 'not me'
           },
         'answer': 'a'
-      },
-      {
-        'term': 'Country',
-        'answers': {
-            'a': 'wrong',
-            'b': 'wrong',
-            'c': 'Country of User',
-            'd': 'wrong'
-          },
-        'answer': 'c'
       }
     ];
 });
